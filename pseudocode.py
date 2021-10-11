911453318227298dd6d3975c3935618fae5e9a08
ImerG12 <77695762+ImerG12@users.noreply.github.com>
2021-10-10 20:36:39 -0700

    Initial commit

---
+++ pseudocode.py
@@ -0,0 +1,18 @@
+personal_info = {
+    'name': "Imer Gramajo",
+    'age': "18",
+    'imaginary_pets':[
+        {
+                'name':"Zeus",
+                'species':"dog",
+                'gender':"male",
+        },
+        {
+                'name':"Bella",
+                'species':"cat",
+                'gender':"female",
+        }
+    ]
+}
+
+print (personal_info ['name'])
